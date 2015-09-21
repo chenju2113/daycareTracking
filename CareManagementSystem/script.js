b8c9c08143d2891f6e12baf08187f843fe6bd9dd
Weilson Wonder <weilson@live.com>
2015-09-21 13:52:28 +0800

    dynamically setting size to div container + refreshing of div

--- CareManagementSystem/script.js
+++ CareManagementSystem/script.js
@@ -19,15 +19,15 @@ var height;
 
 //drawFootPrint(120,250,90);
 
-drawAPs(100,400);
-drawAPs(400,50);
-drawAPs(800,350);
+drawAPs(100, 400);
+drawAPs(400, 50);
+drawAPs(800, 350);
 
 //get all beacons
 $.ajax({
     url: 'http://commandpushingtodevice.mybluemix.net/api/beacon/list',
     type: "GET",
-    success: function (response) {
+    success: function(response) {
         //alert('all clients: ' + response);
         var string = "";
         // this is executed when ajax call finished well
@@ -37,13 +37,13 @@ $.ajax({
             allBeacons[client.id] = client;
         }
         //alert('all clients: ' + string);
-        console.log("going to set interval for all beacons "+jsonData.beacons.length);
+        console.log("going to set interval for all beacons " + jsonData.beacons.length);
         for (var i in allBeacons) {
-            console.log("set interval for "+i);
+            console.log("set interval for " + i);
             setInterval(getClientPos(allBeacons[i]), 1000);
         }
     },
-    error: function (xhr, status, error) {
+    error: function(xhr, status, error) {
         if (xhr.status > 0) alert('got error: ' + status); // status 0 - when load is interrupted
     }
 });
@@ -52,12 +52,12 @@ $.ajax({
 $.ajax({
     url: 'http://commandpushingtodevice.mybluemix.net/api/getData/floors?siteId=z1i30t4p',
     type: "GET",
-    success: function (response) {
+    success: function(response) {
         //alert('all clients: ' + response);
         var string = "";
         // this is executed when ajax call finished well
         var jsonData = JSON.parse(response);
-      
+
         //dun remove the 1st item
         while (floorSelect.options.length > 1) {
             floorSelect.remove(1);
@@ -71,32 +71,40 @@ $.ajax({
             option.value = floor.floorId;
             floorSelect.add(option, i + 1);
         }
-        floorSelect.addEventListener("change", function () {
+        floorSelect.addEventListener("change", function() {
             var image = document.getElementById("myimage");
             if (image) {
                 if (allFloors[floorSelect.options[floorSelect.selectedIndex].value]) {
                     //image.src = allFloors[select.options[select.selectedIndex].value].imageURL;
-                    $.ajax( {
+                    $.ajax({
 
                         // target url/service
                         //url: allFloors[select.options[select.selectedIndex].value].imageURL,
                         url: "getImage.php",
                         type: "POST",
-                        data: ({ url: allFloors[floorSelect.options[floorSelect.selectedIndex].value].imageURL }),
-                        error : function(xhr, ajaxOptions, thrownError) {
+                        data: ({
+                            url: allFloors[floorSelect.options[floorSelect.selectedIndex].value].imageURL
+                        }),
+                        error: function(xhr, ajaxOptions, thrownError) {
                             // reset or whatever
                             console.log("error getting image");
                         },
-                        success: function (img) {
-                            image.src = "data:image/png;base64,"+img;
-                       }
+                        success: function(img) {
+                            image.src = "data:image/png;base64," + img;
+
+                            var imageDivContainer = document.getElementById("mydiv");
+                            imageDivContainer.width = allFloors[floorSelect.options[floorSelect.selectedIndex].value].width;
+                            imageDivContainer.height = allFloors[floorSelect.options[floorSelect.selectedIndex].value].height;
+
+                            $('#mydiv').load(document.URL +  ' #mydiv');
+                        }
                     });
                 }
             }
             getSensors(floorSelect.options[floorSelect.selectedIndex].value);
         });
     },
-    error: function (xhr, status, error) {
+    error: function(xhr, status, error) {
         if (xhr.status > 0) alert('got error: ' + status); // status 0 - when load is interrupted
     }
 });
@@ -106,7 +114,7 @@ function getSensors(floorid) {
     $.ajax({
         url: 'http://commandpushingtodevice.mybluemix.net/api/sensor/list?siteId=z1i30t4p&floorId=' + floorid,
         type: "GET",
-        success: function (response) {
+        success: function(response) {
             //alert('all clients: ' + response);
             var string = "";
             // this is executed when ajax call finished well
@@ -122,7 +130,7 @@ function getSensors(floorid) {
             //alert('all clients: ' + string);
             console.log("got sensors " + jsonData.sensors.length);
         },
-        error: function (xhr, status, error) {
+        error: function(xhr, status, error) {
             if (xhr.status > 0) alert('got error: ' + status); // status 0 - when load is interrupted
         }
     });
@@ -131,46 +139,46 @@ function getSensors(floorid) {
 /*
 var intervalId = setInterval(function() {
     //call $.ajax here
-	$.ajax({
-		url: 'dbConnect.php',
-		type: "POST",
-		data: ({time: "12:00:00"}),
-		success: function (response) {
-			//alert('all clients: ' + response);
-			var string = "";
-			// this is executed when ajax call finished well
-			var jsonData = JSON.parse(response);
-			for (var i = 0; i < jsonData.clients.length; i++) {
-				var client = jsonData.clients[i];
-				console.log(client.clientID);
-				string = string + ", " + client.clientID;
-				var xPos = parseInt(client.x);
-				var yPos = parseInt(client.y);
-				var xPosAfterOffset = xPos + pageXoffset;
-				var yPosAfterOffset = yPos + pageYoffset;
-				drawOldPeople(xPosAfterOffset.toString(), yPosAfterOffset.toString(), client.clientID);
-			}
+    $.ajax({
+        url: 'dbConnect.php',
+        type: "POST",
+        data: ({time: "12:00:00"}),
+        success: function (response) {
+            //alert('all clients: ' + response);
+            var string = "";
+            // this is executed when ajax call finished well
+            var jsonData = JSON.parse(response);
+            for (var i = 0; i < jsonData.clients.length; i++) {
+                var client = jsonData.clients[i];
+                console.log(client.clientID);
+                string = string + ", " + client.clientID;
+                var xPos = parseInt(client.x);
+                var yPos = parseInt(client.y);
+                var xPosAfterOffset = xPos + pageXoffset;
+                var yPosAfterOffset = yPos + pageYoffset;
+                drawOldPeople(xPosAfterOffset.toString(), yPosAfterOffset.toString(), client.clientID);
+            }
 
-			
-			//alert('all clients: ' + string);
-		},
-		error: function (xhr, status, error) {
-			clearInterval(intervalId);
-			alert('error: '+error + " status "+status );
-			// executed if something went wrong during call
-			if (xhr.status > 0) alert('got error: ' + status); // status 0 - when load is interrupted
-		}
-	});
+
+            //alert('all clients: ' + string);
+        },
+        error: function (xhr, status, error) {
+            clearInterval(intervalId);
+            alert('error: '+error + " status "+status );
+            // executed if something went wrong during call
+            if (xhr.status > 0) alert('got error: ' + status); // status 0 - when load is interrupted
+        }
+    });
 }, 3000);
 */
 
 function getClientPos(client) {
     //call $.ajax here
     $.ajax({
-        url: 'http://commandpushingtodevice.mybluemix.net/api/position/full?siteId=z1i30t4p&floorId=7cim0o6e&beaconId='+client.mac,
+        url: 'http://commandpushingtodevice.mybluemix.net/api/position/full?siteId=z1i30t4p&floorId=7cim0o6e&beaconId=' + client.mac,
         type: "GET",
         data: ({}),
-        success: function (response) {
+        success: function(response) {
             console.log(" gotten client " + client.id + "data " + response);
             //alert('all clients: ' + response);
             var string = "";
@@ -179,7 +187,7 @@ function getClientPos(client) {
             drawOldPeople(jsonData.x, jsonData.y, client.id);
             //alert('all clients: ' + string);
         },
-        error: function (xhr, status, error) {
+        error: function(xhr, status, error) {
             clearInterval(intervalId);
             alert('error: ' + error + " status " + status);
             // executed if something went wrong during call
@@ -188,89 +196,88 @@ function getClientPos(client) {
     });
 };
 
-function drawAllFootPrint(mac){
-	$.post("allpoints.php",{
-		mac:mac
-	}).done(function(data){
-		var points = JSON.parse(data);
-		var lastone = [points[0],points[1]];
-		var angle = 90;
-		drawFootPrint(points[0],points[1],angle);
-		var thisone = [];
-		for (var i = 1; i < points.length/2; i ++){
-			thisone = [points[i*2],points[i*2+1]];
-			var dy = -(thisone[1]-lastone[1]);
-			var dx = thisone[0]-lastone[0];
-			if (dx != 0){
-				if (dy > 0){
-					angle = 180/Math.PI*Math.atan(dy/dx);
-				}
-				else {
-					angle = 180/Math.PI*Math.atan(dy/dx)+180;
-				}
-				//alert(points+"\n"+"(thisone y - lastone y):"+thisone[1]+"-"+lastone[1]+"="+dy+"\n(thisone x - lastone x):"+thisone[0]+"-"+lastone[0]+"="+dx+"\n"+angle);
-			}			   		
-		    drawFootPrint(points[i*2],points[i*2+1],angle);
-			lastone = thisone;
-	    }
-	})
-	
-}
-function drawFootPrint(x,y,angle){
-	var image = new Image();
-	image.onload=function(){
-		ctx.save();
-		ctx.translate(x,y);
-		ctx.rotate(angle*Math.PI/180.0);
-		ctx.drawImage(image,0,0,25,25 * image.height / image.width);
-		ctx.restore();
-	}
-	image.src="pair.png";
+function drawAllFootPrint(mac) {
+    $.post("allpoints.php", {
+        mac: mac
+    }).done(function(data) {
+        var points = JSON.parse(data);
+        var lastone = [points[0], points[1]];
+        var angle = 90;
+        drawFootPrint(points[0], points[1], angle);
+        var thisone = [];
+        for (var i = 1; i < points.length / 2; i++) {
+            thisone = [points[i * 2], points[i * 2 + 1]];
+            var dy = -(thisone[1] - lastone[1]);
+            var dx = thisone[0] - lastone[0];
+            if (dx != 0) {
+                if (dy > 0) {
+                    angle = 180 / Math.PI * Math.atan(dy / dx);
+                } else {
+                    angle = 180 / Math.PI * Math.atan(dy / dx) + 180;
+                }
+                //alert(points+"\n"+"(thisone y - lastone y):"+thisone[1]+"-"+lastone[1]+"="+dy+"\n(thisone x - lastone x):"+thisone[0]+"-"+lastone[0]+"="+dx+"\n"+angle);
+            }
+            drawFootPrint(points[i * 2], points[i * 2 + 1], angle);
+            lastone = thisone;
+        }
+    })
+
 }
 
-function drawAPs(x,y){
-	var image = new Image();
-	
-	image.onload=function(){
-		ctx.save();
-		ctx.translate(x,y);
-		ctx.drawImage(image,0,0,50,50 * image.height / image.width);
-		ctx.restore();
-	}
-	image.src="img/wifi_flat_circle_icon.png";
+function drawFootPrint(x, y, angle) {
+    var image = new Image();
+    image.onload = function() {
+        ctx.save();
+        ctx.translate(x, y);
+        ctx.rotate(angle * Math.PI / 180.0);
+        ctx.drawImage(image, 0, 0, 25, 25 * image.height / image.width);
+        ctx.restore();
+    }
+    image.src = "pair.png";
 }
 
-function drawOldPeople(x,y,name){
-	var clientDiv = document.getElementById(name+"_client");
-	if(clientDiv){
-		clientDiv.style.top = y+"px";
-		clientDiv.style.left = x+"px";
-	}else{
-		var myDiv = document.createElement('div');
-		myDiv.className = "parent grow";
-		myDiv.style.position = "absolute";
-		myDiv.style.top = y+"px";
-		myDiv.style.left = x+"px";
-		myDiv.id = name+"_client";
-		var image = document.createElement('img');
-		image.className = "peopleIcon ";
-		image.src="img/human_icon.png";
-		//document.getElementById("iconsHolder").appendChild(image);
-		//image.style.position = "absolute";
-		//image.style.top = x+"px";
-		//image.style.left = y+"px";
-		myDiv.appendChild(image);
-		
-		var popupBG = document.createElement('img');
-		popupBG.src="img/speech_bubble.png";
-		var popupText = document.createElement('p');
-		var linkText = document.createTextNode("Hi, I am "+name);
-		popupText.appendChild(linkText);
-		popupBG.className = "popup popupBG";
-		popupText.className = "popup popupText";
-		myDiv.appendChild(popupBG);
-		myDiv.appendChild(popupText);
-		document.getElementById("iconsHolder").appendChild(myDiv);
-	}
+function drawAPs(x, y) {
+    var image = new Image();
+
+    image.onload = function() {
+        ctx.save();
+        ctx.translate(x, y);
+        ctx.drawImage(image, 0, 0, 50, 50 * image.height / image.width);
+        ctx.restore();
+    }
+    image.src = "img/wifi_flat_circle_icon.png";
 }
 
+function drawOldPeople(x, y, name) {
+    var clientDiv = document.getElementById(name + "_client");
+    if (clientDiv) {
+        clientDiv.style.top = y + "px";
+        clientDiv.style.left = x + "px";
+    } else {
+        var myDiv = document.createElement('div');
+        myDiv.className = "parent grow";
+        myDiv.style.position = "absolute";
+        myDiv.style.top = y + "px";
+        myDiv.style.left = x + "px";
+        myDiv.id = name + "_client";
+        var image = document.createElement('img');
+        image.className = "peopleIcon ";
+        image.src = "img/human_icon.png";
+        //document.getElementById("iconsHolder").appendChild(image);
+        //image.style.position = "absolute";
+        //image.style.top = x+"px";
+        //image.style.left = y+"px";
+        myDiv.appendChild(image);
+
+        var popupBG = document.createElement('img');
+        popupBG.src = "img/speech_bubble.png";
+        var popupText = document.createElement('p');
+        var linkText = document.createTextNode("Hi, I am " + name);
+        popupText.appendChild(linkText);
+        popupBG.className = "popup popupBG";
+        popupText.className = "popup popupText";
+        myDiv.appendChild(popupBG);
+        myDiv.appendChild(popupText);
+        document.getElementById("iconsHolder").appendChild(myDiv);
+    }
+}
